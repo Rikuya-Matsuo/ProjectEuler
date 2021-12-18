1e7f11b1af645492696e2b9cdac7fbc269fcea98
DESKTOP-03G7PES\felid <felid.codas@gmail.com>
2021-12-19 08:01:44 +0900

    システム改修。ファイル追加時に#include文を書き足さなくてもよくなった。

--- ProjectEuler/ProjectEuler/TaskHeader.h
+++ ProjectEuler/ProjectEuler/TaskHeader.h
@@ -1,15 +1,4 @@
 ﻿#pragma once
-#include "Task5.h"
-#include "Task6.h"
-#include "Task7.h"
-#include "Task8.h"
-#include "Task9.h"
-#include "Task10.h"
-#include "Task11.h"
-#include "Task12.h"
-#include "Task13.h"
-#include "Task14.h"
-#include "Task15.h"
 
 #define TASK_CREATE_IMPL() new Task
 #define TASK_CREATE(id) (TASK_CREATE_IMPL()id())
