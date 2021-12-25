8f8ded11caf84893defa938c2842978088ec7b09
DESKTOP-03G7PES\felid <felid.codas@gmail.com>
2021-12-25 11:38:01 +0900

    タスク関連のマクロ定義をTaskHeader.hに集約した。

--- ProjectEuler/ProjectEuler/TaskHeader.h
+++ ProjectEuler/ProjectEuler/TaskHeader.h
@@ -2,3 +2,15 @@
 
 #define TASK_CREATE_IMPL(id) (new Task##id())
 #define TASK_CREATE(id) TASK_CREATE_IMPL(id)
+
+// タスクID。ここを変更して動かすタスクを変える。
+#define TASK_ID 15
+
+// タスクcppファイルのインクルード
+#define TO_STRING(x) #x
+#define TO_STRING_AFTER_EXTEND(x) TO_STRING(x)
+
+#define TASK_FILENAME_IMPL(id) Task##id##.h
+#define TASK_FILENAME(id) TASK_FILENAME_IMPL(id)
+
+#include TO_STRING_AFTER_EXTEND(TASK_FILENAME(TASK_ID))
--- ProjectEuler/ProjectEuler/main.cpp
+++ ProjectEuler/ProjectEuler/main.cpp
@@ -1,15 +1,5 @@
 ﻿#include "TaskHeader.h"
 
-#define TASK_ID 15
-
-#define TO_STRING(x) #x
-#define TO_STRING_AFTER_EXTEND(x) TO_STRING(x)
-
-#define TASK_FILENAME_IMPL(id) Task##id##.h
-#define TASK_FILENAME(id) TASK_FILENAME_IMPL(id)
-
-#include TO_STRING_AFTER_EXTEND(TASK_FILENAME(TASK_ID))
-
 int main(int argc, char ** argv)
 {
 	std::cout << "Current Task ID : " << TASK_ID << "\nStart run?(cancel : 0, run : natural number) : ";
