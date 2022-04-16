07a26ebfa7d3ebb089c4bac32e498d859d6bc91e
DESKTOP-03G7PES\felid <felid.codas@gmail.com>
2022-04-16 20:17:30 +0900

    問題20を解答

---
+++ ProjectEuler/ProjectEuler/Task20.cpp
@@ -0,0 +1,70 @@
+﻿#include "Task20.h"
+
+Task20::Task20()
+{
+}
+
+Task20::~Task20()
+{
+}
+
+void Task20::Run()
+{
+	const uint16 fuctorialTarget = 100;
+
+	LongNum calcResult(1);
+	for (uint16 i = 0; i < fuctorialTarget; ++i)
+	{
+		calcResult *= (i + 1);
+	}
+
+	size_t ans = calcResult.CalcDigitSum();
+
+	STD_COUT("Answer : ", ans);
+}
+
+Task20::LongNum& Task20::LongNum::operator*=(sint32 x)
+{
+	for (auto & num : mNumber)
+	{
+		num *= x;
+	}
+	Update();
+
+	return *this;
+}
+
+size_t Task20::LongNum::CalcDigitSum() const
+{
+	size_t ret = 0;
+	for (auto num : mNumber)
+	{
+		while (num > 0)
+		{
+			ret += num % 10;
+			num /= 10;
+		}
+	}
+
+	return ret;
+}
+
+void Task20::LongNum::Update()
+{
+	static const UnitType unitLim = (UnitType)(pow(10, Task20::LongNum::mDigitOfUnit));
+
+	// 繰り上がり
+	UnitType carry = 0;
+	for (auto & num : mNumber)
+	{
+		num += carry;
+
+		carry = num / unitLim;
+		num %= unitLim;
+	}
+
+	if (carry)
+	{
+		mNumber.emplace_back(carry);
+	}
+}
---
+++ ProjectEuler/ProjectEuler/Task20.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+#include "TaskBase.h"
+
+class Task20 : public TaskBase
+{
+public:
+	Task20();
+	~Task20();
+
+	void Run() override;
+
+private:
+	class LongNum;
+};
+
+class Task20::LongNum
+{
+public:
+	using UnitType = sint32;
+	LongNum(UnitType initNum) { *this =initNum; }
+
+	LongNum& operator=(UnitType num)
+	{
+		mNumber.clear();
+		mNumber.emplace_back(num);
+		return *this;
+	}
+	LongNum& operator*=(sint32 x);
+
+	size_t CalcDigitSum() const;
+
+private:
+	static const uint8 mDigitOfUnit = 6;
+
+	std::list<UnitType> mNumber;
+
+	void Update();
+};
