22fc93136bdd430a5635e98b0d76734b47a2dc6f
DESKTOP-03G7PES\felid <felid.codas@gmail.com>
2022-10-09 12:36:59 +0900

    問題25を解答

---
+++ ProjectEuler/ProjectEuler/Task25.cpp
@@ -0,0 +1,83 @@
+﻿#include "Task25.h"
+
+namespace
+{
+	using LargeInt = Task25::LargeInt;
+}
+
+Task25::Task25()
+{
+}
+
+Task25::~Task25()
+{
+}
+
+void Task25::Run()
+{
+	uint32 index = 0;
+	do
+	{
+		++index;
+		if (index <= 2)
+		{
+			mFibonacciSeq[index] = 1;
+			continue;
+		}
+
+		mFibonacciSeq[index] = mFibonacciSeq[index - 1] + mFibonacciSeq[index - 2];
+	} while (mFibonacciSeq[index].getDigitNum() < 1000);
+
+	COUT_ANSWER(index);
+}
+
+
+
+LargeInt LargeInt::operator+(const LargeInt & rhs)
+{
+	LargeInt sum;
+	sum.mUnits.clear();
+	const auto lhsUnitSize = this->mUnits.size();
+	const auto rhsUnitSize = rhs.mUnits.size();
+	const auto loopNum = (lhsUnitSize > rhsUnitSize ? lhsUnitSize : rhsUnitSize);
+	for (size_t i = 0; i < loopNum; ++i)
+	{
+		const uint32 lhsUnit = (i < lhsUnitSize) ? this->mUnits[i] : 0;
+		const uint32 rhsUnit = (i < rhsUnitSize) ? rhs.mUnits[i] : 0;
+
+		sum.mUnits.emplace_back(lhsUnit + rhsUnit);
+	}
+	sum.Update();
+	return sum;
+}
+
+LargeInt& LargeInt::operator=(uint32 rhs)
+{
+	mUnits.clear();
+	mUnits.emplace_back(rhs);
+	Update();
+	return *this;
+}
+
+void LargeInt::Update()
+{
+	uint8 carry = 0;
+	for (auto& unit : mUnits)
+	{
+		unit += carry;
+		carry = unit / BILLION;
+		unit %= BILLION;
+	}
+	if (carry) { mUnits.emplace_back(carry); }
+
+	uint8 backUnitDigitNum = 0;
+	uint32 backUnitBuf = mUnits.back();
+	while (backUnitBuf)
+	{
+		++backUnitDigitNum;
+		backUnitBuf /= 10;
+	}
+
+	// ユニット１つが10億未満なので９桁
+	mDigitNum = ((uint32)mUnits.size() - 1) * 9 + backUnitDigitNum;
+}
---
+++ ProjectEuler/ProjectEuler/Task25.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+#include "TaskBase.h"
+
+class Task25 : public TaskBase
+{
+public:
+	Task25();
+	~Task25();
+
+	class LargeInt;
+
+	void Run() override;
+
+private:
+	std::unordered_map<uint32, LargeInt> mFibonacciSeq;
+};
+
+class Task25::LargeInt
+{
+public:
+	LargeInt(uint32 num = 0)
+	{
+		*this = num;
+	}
+
+	LargeInt operator+(const LargeInt& rhs);
+	LargeInt operator+(uint32 rhs)
+	{
+		LargeInt rhsLI = rhs;
+		return (*this + rhsLI);
+	}
+	LargeInt& operator=(uint32 rhs);
+
+	uint32 getDigitNum() const { return mDigitNum; }
+
+private:
+	std::vector<uint32> mUnits;
+	uint32 mDigitNum;
+
+private:
+	void Update();
+};
